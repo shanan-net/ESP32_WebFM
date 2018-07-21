f440f0308e706362128b67935abdb1aa5ddbc026
Shaonan Ruan <shanan.net@qq.com>
2018-07-21 11:58:56 +0800

    Update rda5820.h

--- Software/GD32/rda5820.h
+++ Software/GD32/rda5820.h
@@ -13,15 +13,20 @@
 #include "gd32f1x0.h"
 #include <stdbool.h>
 
+#define SEEK_OK	0
+#define SEEK_NG	1
+
 void RDA5820_RX_Intialization(void);
 void RDA5820_TX_Intialization(void);
 void RDA5820_TXI2S_Intialization(void);
 void RDA5820_DAC_Intialization(void);
 void RDA5820_Deinit(void);
-void RDA5820_Mute(unsigned char mute);
+void RDA5820_MuteHW(unsigned char mute);
+void RDA5820_MuteSW(unsigned char mute);
 uint32_t RDA5820_FreqToChan(uint32_t freq);
 uint32_t RDA5820_ChanToFreq(uint32_t chan);
 void RDA5820_SetFreq(uint32_t curFreq);
+uint8_t RDA5820_Seek(uint8_t dir);
 bool RDA5820_ValidStop(uint32_t freq);
 uint8_t RDA5820_GetSigLvl(uint32_t curf);
 void RDA5820_SetVolumeLevel(unsigned char level);
